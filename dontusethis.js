82f40f43c56fdd138fd9e211caf43d0a40618494
Daniel Chu <dchu0518@gmail.com>
2017-03-25 12:44:00 -0700

    movement stuff almost good ag except not really

--- dontusethis.js
+++ dontusethis.js
@@ -1,34 +1,3 @@
-var renderer = PIXI.autoDetectRenderer(750, 750, {backgroundColor : 0xffffff});
-document.body.appendChild(renderer.view);
-var stage = new PIXI.Container();
-
-var backGrid = PIXI.Sprite.fromImage("images/grid.jpg");
-stage.addChild(backGrid)
-var grid = [];
-var grid_sprite = [];
-for(var x = 0; x < 9; x++)
-{
-  grid.push([]);
-  grid_sprite.push([]);
-  for(var y = 0; y < 9; y++)
-  {
-    grid[x].push({});
-    grid_sprite[x].push(null);
-    grid[x][y].pieceid = 0;
-    if(y < 4)
-    {
-      grid[x][y].player = 0;
-    }
-    else
-    {
-      grid[x][y].player = 1;
-    }
-  }
-}
-
-createSprites();
-
-
 function createSprites()
 {
     
@@ -51,16 +20,17 @@ function createSprites()
             piece.state = "alive";
             piece.clicks = 0;
             piece.interactive = true;
-            piece.hitArea = new PIXI.Rectangle(0, 0, 80, 80);
+            piece.hitArea = new PIXI.Rectangle(piece.x, piece.y, 78, 78);
             grid_sprite[x][y] = piece;
 
-            stage.addChild(piece);
+            app.stage.addChild(piece);
         }
     }
 }
 
 function updateSprites()
-{ 
+{
+    
     var pieceimages = ["/images/lance.png", "/images/knight.png", "/images/general_silver.png", "/images/general_gold.png", "/images/king_low.png", "/images/bishop.png", "/images/rook.png", "/images/pawn.png", "/images/bishop_promoted.png", "/images/dragon.png", "/images/king_high.png", "/images/knight_promoted.png", "/images/lance_promoted.png", "/images/pawn_promoted.png", "/images/silver_promoted.png"]
     
     for(var x = 0; x < grid_sprite.length; x++)
@@ -69,7 +39,7 @@ function updateSprites()
         {
           var piece = grid_sprite[x][y];
           if(grid_sprite[x][y]!==null){
-            stage.removeChild(grid_sprite[x][y]);
+            app.stage.removeChild(grid_sprite[x][y]);
           }
 
             piece = PIXI.Sprite.fromImage(pieceimages[grid[x][y].player]);
@@ -79,7 +49,7 @@ function updateSprites()
             piece.state = "alive";
             piece.clicks = 0;
             piece.interactive = true;
-            piece.hitArea = new PIXI.Rectangle(0, 0, 80, 80);
+            piece.hitArea = new PIXI.Rectangle(piece.x, piece.y, 78, 78);
             grid_sprite[x][y] = piece;
 
 
@@ -88,15 +58,15 @@ function updateSprites()
               console.log("mouseover");
               piece.state = "mouseover";
               while(piece.clicks == 1){
-                stage.removeChild(piece);
+                app.stage.removeChild(piece);
                 piece.state = "moving";
                 movePiece(piece);
 
-                stage.addChild(piece);
+                app.stage.addChild(piece);
                 console.log(piece.x,piece.y);
                 console.log(grid_sprite[x]);
                 console.log(x,y);
-                grid_sprite[x][y] = piece;
+                grid_sprite[piece.x][piece.y] = piece;
 
                 console.log(piece.x + ", " + piece.y);
                 console.log("clicks:" + piece.clicks);
@@ -106,7 +76,7 @@ function updateSprites()
             piece.click = function(mouseData)
             {
               piece.clicks++;
-              stage.removeChild(piece);
+              app.stage.removeChild(piece);
               console.log("mouseClick");
               if(piece.clicks == 2){
                 movePiece(piece);
@@ -114,18 +84,10 @@ function updateSprites()
               }
             }
 
-            stage.addChild(piece);
+            app.stage.addChild(piece);
         }
     }
 }
-
-document.addEventListener('mousemove', checkMovement, false);
-document.addEventListener('click', checkMovement, false);
-
-function checkMovement(){
-
-}
-
 
 
 function movePiece(obj){
@@ -137,14 +99,3 @@ function movePiece(obj){
   console.log("x:" + e.clientX + ", y:" + e.clientY);
 
 }
-// start animating
-animate();
-
-function animate() {
-  requestAnimationFrame(animate);
-  // render the root container
-  renderer.render(stage);
-};
-
-
-updateSprites();
